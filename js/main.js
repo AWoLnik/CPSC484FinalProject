807ae8b500de69cb35fb8817a1876826a608b9b8
Nathan Ahn <nathancahn@gmail.com>
2021-05-11 02:49:51 -0400

    made background better fit the CEID, data smoothing, moved leaderboard to canvas, added jumping jacks

--- js/main.js
+++ js/main.js
@@ -1,1 +1,288 @@
 //JavaScript code for general stuff (feel free to create extra files)
+// canvas object
+var c;
+var ctx;
+var personRadiusCM;
+var origin;
+const exerciseFormatting = {
+    "squat": "Squats",
+    "pushup": "Push-Ups",
+    "situp": "Sit-Ups",
+    "jumpingjack": "Jumping-Jacks",
+}
+const fontScale = 1;
+function start(){
+    frames.start();
+    c = document.getElementById("draw");
+    ctx = c.getContext("2d");
+    
+    // all real-world units are in mm unless denoted by CM
+    personRadiusCM = 30;
+    // origin (x,y) 
+    origin = [c.width/2, c.height/2];
+}
+var frames = {
+  socket: null,
+  start: function() {
+    resizeCanvas();
+    window.onresize = resizeCanvas;
+    var ip = window.location.search;
+    ip = ip.split('ip=')[1];
+    console.log(`connecting to ${ip}`);
+    // websocket connection location
+    var url = "ws://" + ip + "/frames";
+
+
+    // subscribe to the /frames data
+    frames.socket = new WebSocket(url);
+    frames.socket.onmessage = function(event) {
+      let data = JSON.parse(event.data);
+      handleData(data);
+      handleCanvas(data);
+      // clear the canvas
+    }
+  }
+};
+function handleCanvas(data){ //Draws people to canvas and all that
+  ctx.clearRect(0, 0, c.width, c.height);
+  ctx.textAlign = "center";    
+  ctx.font = `${fontScale*25}px Arial`
+  //Gradient
+  var colors = [230, 160];
+  var gradient = ctx.createLinearGradient(0, 0, 0, c.height);
+  gradient.addColorStop(0, `rgb(${colors[0]}, ${colors[0]}, ${colors[0]})`);
+  gradient.addColorStop(1, `rgb(${colors[1]}, ${colors[1]}, ${colors[1]})`);
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0, 0, c.width, c.height);
+  //Leaderboard
+  ctx.fillStyle = "aliceBlue";
+  const leaderboardPos = [10, 10];
+  const leaderboardDim = [fontScale*300, fontScale*(50+40*Object.keys(maxCount).length)];
+  const textPos = [leaderboardPos[0]+leaderboardDim[0]/2, leaderboardPos[1]];
+  ctx.fillRect(leaderboardPos[0], leaderboardPos[1], leaderboardDim[0], leaderboardDim[1]);
+  ctx.strokeStyle = "rgb(120, 120, 120)";
+  ctx.lineWidth=1;
+  ctx.rect(leaderboardPos[0], leaderboardPos[1], leaderboardDim[0], leaderboardDim[1]);
+  ctx.stroke();
+  ctx.fillStyle = "black";
+  let texts = ["Leaderboard"];
+  for(let key in maxCount){
+      texts.push(`${exerciseFormatting[key]}: ${maxCount[key]}`);
+  }
+  for(let i = 0; i < texts.length; i++){
+    let x = textPos[0];
+    if(i > 0){
+        x = leaderboardPos[0]+fontScale*10
+        ctx.textAlign = "left";    
+    }
+    ctx.fillText(texts[i], x, fontScale*30+textPos[1]+fontScale*40*i);
+  }
+  ctx.textAlign = "center";    
+  // draw the camera on the top of the screen
+  drawEnv(ctx, origin);
+
+  var i = 0;
+  var person_scale = 0.25;
+  ctx.font = `${fontScale*12}px Arial`
+  if (data.people) {
+    var num_people = Object.keys(data.people).length;
+    //console.log(num_people);
+    for (const [idx, person] of Object.entries(data.people)) {
+      ctx.lineWidth = 3;        
+      keypoints = count[person.idx].smoothedKeypoints;
+      var countX = (keypoints.LShoulder[0]*person_scale * (-1) + origin[0] + keypoints.RShoulder[0]*person_scale * (-1) + origin[0])/2;
+      var countY = (keypoints.LShoulder[1]*person_scale + origin[1] + keypoints.RShoulder[1]*person_scale + origin[1])/2 - 50;
+
+      var mostProgress = ["None", 0];
+      for(let poseKey in poseData){
+        let completeness = count[person.idx][poseKey].count+(1+count[person.idx][poseKey].lastPosition)/poseData[poseKey].length; //Scaling each exercise to each other in stages
+        if(completeness > mostProgress[1]){
+          mostProgress = [poseKey, completeness];
+        }
+      }
+      var headText = ""
+      if(mostProgress[0] == "None"){
+        headText = "Try doing a squat or jumping jack!";
+      }
+      else{
+        headText = `${exerciseFormatting[mostProgress[0]]}: ${count[person.idx][mostProgress[0]].count}`;
+      }
+      var colorScale = mostProgress[1]-Math.floor(mostProgress[1]); //Gets the progress amount
+      ctx.strokeStyle = ctx.fillStyle = `rgb(0, ${colorScale}, 0)`;
+      if(countX && countY){
+        ctx.fillText(headText, countX, countY);
+      }
+
+      ctx.beginPath();
+
+      if(keypoints.LShoulder && keypoints.RShoulder){
+        canvas_line(ctx,
+          keypoints.LShoulder[0]*person_scale * (-1) + origin[0],
+          keypoints.LShoulder[1]*person_scale + origin[1],
+          keypoints.RShoulder[0]*person_scale * (-1) + origin[0],
+          keypoints.RShoulder[1]*person_scale + origin[1]);
+      }
+      if(keypoints.LShoulder && keypoints.LHip){
+        canvas_line(ctx,
+          keypoints.LShoulder[0]*person_scale * (-1) + origin[0],
+          keypoints.LShoulder[1]*person_scale + origin[1],
+          keypoints.LHip[0]*person_scale * (-1) + origin[0],
+          keypoints.LHip[1]*person_scale + origin[1]);
+      }
+      if(keypoints.RShoulder && keypoints.RHip){
+        canvas_line(ctx,
+          keypoints.RShoulder[0]*person_scale * (-1) + origin[0],
+          keypoints.RShoulder[1]*person_scale + origin[1],
+          keypoints.RHip[0]*person_scale * (-1) + origin[0],
+          keypoints.RHip[1]*person_scale + origin[1]);
+      }
+      if(keypoints.LHip && keypoints.RHip){
+        canvas_line(ctx,
+          keypoints.LHip[0]*person_scale * (-1) + origin[0],
+          keypoints.LHip[1]*person_scale + origin[1],
+          keypoints.RHip[0]*person_scale * (-1) + origin[0],
+          keypoints.RHip[1]*person_scale + origin[1]);
+      }
+      if(keypoints.LHip && keypoints.LKnee){
+        canvas_line(ctx,
+          keypoints.LHip[0]*person_scale * (-1) + origin[0],
+          keypoints.LHip[1]*person_scale + origin[1],
+          keypoints.LKnee[0]*person_scale * (-1) + origin[0],
+          keypoints.LKnee[1]*person_scale + origin[1]);
+      }
+      if(keypoints.RHip && keypoints.RKnee){
+        canvas_line(ctx,
+          keypoints.RHip[0]*person_scale * (-1) + origin[0],
+          keypoints.RHip[1]*person_scale + origin[1],
+          keypoints.RKnee[0]*person_scale * (-1) + origin[0],
+          keypoints.RKnee[1]*person_scale + origin[1]);
+      }
+      if(keypoints.LKnee && keypoints.LAnkle){
+        canvas_line(ctx,
+          keypoints.LKnee[0]*person_scale * (-1) + origin[0],
+          keypoints.LKnee[1]*person_scale + origin[1],
+          keypoints.LAnkle[0]*person_scale * (-1) + origin[0],
+          keypoints.LAnkle[1]*person_scale + origin[1]);
+      }
+      else if(keypoints.LAnkle && keypoints.LHip){ //Ignores knees if it has to
+        canvas_line(ctx,
+          keypoints.LAnkle[0]*person_scale * (-1) + origin[0],
+          keypoints.LAnkle[1]*person_scale + origin[1],
+          keypoints.LHip[0]*person_scale * (-1) + origin[0],
+          keypoints.LHip[1]*person_scale + origin[1]);
+      }
+      if(keypoints.RKnee && keypoints.RAnkle){
+        canvas_line(ctx,
+          keypoints.RKnee[0]*person_scale * (-1) + origin[0],
+          keypoints.RKnee[1]*person_scale + origin[1],
+          keypoints.RAnkle[0]*person_scale * (-1) + origin[0],
+          keypoints.RAnkle[1]*person_scale + origin[1]);
+      }
+      else if(keypoints.RAnkle && keypoints.RHip){
+        canvas_line(ctx,
+          keypoints.RAnkle[0]*person_scale * (-1) + origin[0],
+          keypoints.RAnkle[1]*person_scale + origin[1],
+          keypoints.RHip[0]*person_scale * (-1) + origin[0],
+          keypoints.RHip[1]*person_scale + origin[1]);
+      }
+      if(keypoints.LShoulder && keypoints.LElbow){
+        canvas_line(ctx,
+          keypoints.LShoulder[0]*person_scale * (-1) + origin[0],
+          keypoints.LShoulder[1]*person_scale + origin[1],
+          keypoints.LElbow[0]*person_scale * (-1) + origin[0],
+          keypoints.LElbow[1]*person_scale + origin[1]);
+      }
+      if(keypoints.RShoulder && keypoints.RElbow){
+        canvas_line(ctx,
+          keypoints.RShoulder[0]*person_scale * (-1) + origin[0],
+          keypoints.RShoulder[1]*person_scale + origin[1],
+          keypoints.RElbow[0]*person_scale * (-1) + origin[0],
+          keypoints.RElbow[1]*person_scale + origin[1]);
+      }
+      if(keypoints.LElbow && keypoints.LWrist){
+        canvas_line(ctx,
+          keypoints.LElbow[0]*person_scale * (-1) + origin[0],
+          keypoints.LElbow[1]*person_scale + origin[1],
+          keypoints.LWrist[0]*person_scale * (-1) + origin[0],
+          keypoints.LWrist[1]*person_scale + origin[1]);
+      }
+      if(keypoints.RElbow && keypoints.RWrist){
+        canvas_line(ctx,
+          keypoints.RElbow[0]*person_scale * (-1) + origin[0],
+          keypoints.RElbow[1]*person_scale + origin[1],
+          keypoints.RWrist[0]*person_scale * (-1) + origin[0],
+          keypoints.RWrist[1]*person_scale + origin[1]);
+      }
+      ctx.stroke();
+    }
+  }
+}
+const resizeCanvas = () => { //from: https://stackoverflow.com/questions/4288253/html5-canvas-100-width-height-of-viewport
+    canvas = document.getElementById("draw");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+// Helper Functions
+
+// Convert MM to CM
+function toCM(mm) {
+  return mm/10;
+}
+
+// Draw an arrow
+// from: https://stackoverflow.com/questions/808826/draw-arrow-on-canvas-tag#answer-6333775
+function canvas_arrow(context, fromx, fromy, tox, toy) {
+  var headlen = 10; // length of head in pixels
+  var dx = tox - fromx;
+  var dy = toy - fromy;
+  var angle = Math.atan2(dy, dx);
+  context.moveTo(fromx, fromy);
+  context.lineTo(tox, toy);
+  context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
+  context.moveTo(tox, toy);
+  context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+}
+
+function canvas_line(context, fromx, fromy, tox, toy) {
+  context.moveTo(fromx, fromy);
+  context.lineTo(tox, toy);
+}
+
+// Setup an array of colors
+// var tc = tinycolor({
+//   r: Math.floor(Math.random() * 0xFF),
+//   g: Math.floor(Math.random() * 0xFF),
+//   b: Math.floor(Math.random() * 0xFF)
+// });
+// colors = [];
+// var parts = 2 + Math.floor(Math.random() * 5);
+// for (var i = 0; i < parts; i++) {
+//   tc = tc.spin(360 / parts);
+//   colors.push('#' + tc.toHex());
+// }
+
+// Draw the environment
+function drawEnv(ctx, origin) {
+  // var cameraSizeCM = [15, 5];
+
+  // ctx.strokeStyle = ctx.fillStyle = '#333333';
+  // ctx.fillRect(origin[0], origin[1]-cameraSizeCM[1]/2, cameraSizeCM[0], cameraSizeCM[1])
+
+  // ctx.font = "25px Arial";
+  // ctx.fillText("x", 90, 20);
+  // ctx.fillText("y", 8, 100);
+  // ctx.beginPath();
+  // canvas_arrow(ctx, 14, 14, 14, 80);
+  // canvas_arrow(ctx, 14, 14, 80, 14);
+  // ctx.stroke();
+
+  // ctx.beginPath();
+  // ctx.strokeStyle = ctx.fillStyle = '#0000FF';
+  // canvas_arrow(ctx, origin[0]+cameraSizeCM[0]/2, 1.5, origin[0]+cameraSizeCM[0]/2, 20);
+  // ctx.stroke();
+
+  // ctx.beginPath();
+  // ctx.strokeStyle = ctx.fillStyle = '#FF0000';
+  // canvas_arrow(ctx, origin[0]+cameraSizeCM[0]/2, 1.5, origin[0]+cameraSizeCM[0]/2-20, 1.5);
+  // ctx.stroke();
+}
--- js/poseDetection.js
+++ js/poseDetection.js
@@ -4,29 +4,72 @@
 - Add new poses
 - Perhaps some noise suppression by averaging over keypoints?
 */
-const error = 15; //Margin for how close a pose needs to be to count. In degrees
+const storedKeypoints = 5; //Number of keypoints stored for each person
+const error = 20; //Margin for how close a pose needs to be to count. In degrees
 const tickFade = 20; //Number of ticks before a person ID is removed (I'm not sure how noisy people existence is, so I'm doing this to hold on to them for a bit longer. Probably not necessary?)
 const poseData = { //Angles for each of the exercises. Each set of angles represents the required joint positions to meet that stage of the exercise. If a joint doesn't exist, the pose does not care about that joint (ex: arms do not matter for squatting)
     squat: [
         {
             lHip: 160,
             rHip: 160,
-            lKnee: 160,
-            rKnee: 160,
+            lKnee: 140,
+            rKnee: 140,
         },
         {
-            lHip: 115,
-            rHip: 115,
-            lKnee: 125,
-            rKnee: 125,
+            lHip: 120,
+            rHip: 120,
+            lKnee: 105,
+            rKnee: 105,
+        }
+    ],
+    jumpingjack: [
+        {
+            lElbow: 170,
+            rElbow: 170,
+            lShoulder: 20,
+            rShoulder: 20,
+            lHip: 160,
+            rHip: 160,
+            lKnee: 140,
+            rKnee: 140,
+        },
+        {
+            lElbow: 140, 
+            rElbow: 140, 
+            lShoulder: 75,
+            rShoulder: 75,
+            lHip: 150,
+            rHip: 150,
+            lKnee: 140, 
+            rKnee: 140, 
         }
     ]
+}
+const blankKeypoints = {
+    Nose: null,
+    LEye: null,
+    REye: null,
+    LEar: null,
+    REar: null,
+    LShoulder: null,
+    RShoulder: null,
+    LElbow: null,
+    RElbow: null,
+    LWrist: null,
+    RWrist: null,
+    LHip: null,
+    RHip: null,
+    LKnee: null,
+    RKnee: null,
+    LAnkle: null,
+    RAnkle: null,
 }
 var count = {};
 var maxCount = {
     squat: 0,
-    pushup: 0,
-    situp: 0,
+    //pushup: 0,
+    //situp: 0,
+    jumpingjack: 0,
 };
 const handleData = (data) => { //Called by main, where the connection to the socket is made
     if(data.people){
@@ -37,7 +80,7 @@ const handleData = (data) => { //Called by main, where the connection to the soc
     for(let personID in count){ //Removes people if necessary
         count[personID].tickFade--;
         if(count[personID].tickFade == 0){
-            //console.log(`Deleting ${personID}`);
+            console.log(`Deleting ${personID}`);
             delete(count[personID]);
         }
     }
@@ -46,6 +89,11 @@ const handlePerson = (person) => {
     if(!count[person.idx]){ //Checks if person exists in the count dictionary yet, if not, adds them
         count[person.idx] = {
             tickFade: tickFade,
+            keypoints: [],
+            smoothedKeypoints: blankKeypoints,
+        }
+        for(let i = 0; i < storedKeypoints; i++){
+            count[person.idx].keypoints[i] = null;
         }
         for(let poseKey in poseData){ //Initializes each of the available exercises to keep track
             count[person.idx][poseKey] = {
@@ -55,17 +103,43 @@ const handlePerson = (person) => {
         }
         //console.log(`Added new person ${person.idx}`);
     }
+    for(let i = 0; i < storedKeypoints-1; i++){
+        count[person.idx].keypoints[i+1] = count[person.idx].keypoints[i];
+    }
+    count[person.idx].keypoints[0] = person.keypoints;
+    for(let point in count[person.idx].smoothedKeypoints){
+        let usableData = [];
+        for(let i = 0; i < storedKeypoints; i++){
+            if(count[person.idx].keypoints[i][point]){
+                usableData.push(count[person.idx].keypoints[i][point]);
+            }
+        }
+        let weightedData = [0,0,0];
+        let avgDenominator = 0;
+        for(let i = 0; i < usableData.length; i++){
+            for(let j = 0; j <= 3; j++){
+                weightedData[j] += Math.pow(usableData.length-i, 2)*usableData[i][j]; //Weighting factor so most recent ones matter more
+            }
+            avgDenominator += Math.pow(usableData.length-i, 2);
+        }
+        for(let j = 0; j <= 3; j++){
+            weightedData[j] /= avgDenominator;
+        }
+        count[person.idx].smoothedKeypoints[point] = weightedData;
+    }
     count[person.idx].tickFade = Object.assign(tickFade); //Resets the tickFade for all the people in frame
+    //Updating keypoints of person
     const joints = { //Calculates all the angles (null joint if a keypoint doesn't exist)
-        lElbow: calculateAngle(person.keypoints.LElbow, person.keypoints.LWrist, person.keypoints.LShoulder), 
-        rElbow: calculateAngle(person.keypoints.RElbow, person.keypoints.RWrist, person.keypoints.RShoulder),
-        lShoulder: calculateAngle(person.keypoints.LShoulder, person.keypoints.LElbow, person.keypoints.LHip),
-        rShoulder: calculateAngle(person.keypoints.RShoulder, person.keypoints.RElbow, person.keypoints.RHip),
-        lHip: calculateAngle(person.keypoints.LHip, person.keypoints.LShoulder, person.keypoints.LKnee),
-        rHip: calculateAngle(person.keypoints.RHip, person.keypoints.RShoulder, person.keypoints.RKnee),
-        lKnee: calculateAngle(person.keypoints.LKnee, person.keypoints.LHip, person.keypoints.LAnkle),
-        rKnee: calculateAngle(person.keypoints.RKnee, person.keypoints.RHip, person.keypoints.RAnkle),
+        lElbow: calculateAngle(count[person.idx].smoothedKeypoints.LElbow, count[person.idx].smoothedKeypoints.LWrist, count[person.idx].smoothedKeypoints.LShoulder), 
+        rElbow: calculateAngle(count[person.idx].smoothedKeypoints.RElbow, count[person.idx].smoothedKeypoints.RWrist, count[person.idx].smoothedKeypoints.RShoulder),
+        lShoulder: calculateAngle(count[person.idx].smoothedKeypoints.LShoulder, count[person.idx].smoothedKeypoints.LElbow, count[person.idx].smoothedKeypoints.LHip),
+        rShoulder: calculateAngle(count[person.idx].smoothedKeypoints.RShoulder, count[person.idx].smoothedKeypoints.RElbow, count[person.idx].smoothedKeypoints.RHip),
+        lHip: calculateAngle(count[person.idx].smoothedKeypoints.LHip, count[person.idx].smoothedKeypoints.LShoulder, count[person.idx].smoothedKeypoints.LKnee),
+        rHip: calculateAngle(count[person.idx].smoothedKeypoints.RHip, count[person.idx].smoothedKeypoints.RShoulder, count[person.idx].smoothedKeypoints.RKnee),
+        lKnee: calculateAngle(count[person.idx].smoothedKeypoints.LKnee, count[person.idx].smoothedKeypoints.LHip, count[person.idx].smoothedKeypoints.LAnkle),
+        rKnee: calculateAngle(count[person.idx].smoothedKeypoints.RKnee, count[person.idx].smoothedKeypoints.RHip, count[person.idx].smoothedKeypoints.RAnkle),
     };
+    console.log(joints)
     for(let poseKey in poseData){ //Iterates through each exercise and each pose in that exercise
         for(let poseStage = 0; poseStage < poseData[poseKey].length; poseStage++){ //Iterating through each pose in the exercise
             let matchPose = true; //Assumes matches until it's not
